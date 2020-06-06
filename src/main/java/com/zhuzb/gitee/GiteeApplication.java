ee21923264fc5d772abc9c409c884d6d6d95f6c5
zhuzhengbin <2382798239@qq.com>
2020-06-06 22:59:45 +0800

    【开发】项目初始化

---
+++ src/main/java/com/zhuzb/gitee/GiteeApplication.java
@@ -0,0 +1,13 @@
+package com.zhuzb.gitee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiteeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiteeApplication.class, args);
+    }
+
+}
